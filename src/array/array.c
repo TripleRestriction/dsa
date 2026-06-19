5245464fd9b7505f5a37f75c0396a6fe72a3bbe7
TripleRestriction <208179266+TripleRestriction@users.noreply.github.com>
2026-06-19 12:57:09 +0530

    updatew

--- src/array/array.c
+++ src/array/array.c
@@ -13,8 +13,17 @@ void createArray(myArray *a, size_t tSize, size_t uSize){
 	a->ptr = (int *)malloc(tSize*sizeof(int));
 }
 
+void showArray(myArray * a){
+	for (size_t i = 0; i < a->usedSize; i++){
+		printf("%d\n", a->ptr[i]);
+	}
+}
+
 int main(int argc, char *argv[]){
 	myArray array;
-	createArray(&array, 100, 20);
+	createArray(&array, 10, 2);
+	array.ptr[0] = 69;
+	array.ptr[1] = 67;
+	showArray(&array);
 	return 0;
 }
