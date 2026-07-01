762400a00ab6ee9610108d3b34b18834e2cb5347
TripleRestriction <208179266+TripleRestriction@users.noreply.github.com>
2026-07-01 21:39:18 +0530

    potatoes

--- src/binarytree/binarySearchTree.c
+++ src/binarytree/binarySearchTree.c
@@ -2,3 +2,7 @@
 // all values in the left sub tree are less than the value of parent node
 // all values in the right sub tree are greater than the value of parent node
 // no duplicate values
+// fact:
+// inorder traversal of a bst gives an ascending sorted array
+// so you can just calculate the inorder and if it is sorted ascending 
+// then the structure is a bst
